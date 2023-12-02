8105e22328592cc1ad497dc02626bd9b8bcb5f78
trevortomlin <trevortomlin2@outlook.com>
2023-12-02 06:21:47 -0800

    Day 2 complete

---
+++ src/bin/day2.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+
+use rp_pico::entry;
+use embedded_hal::digital::v2::OutputPin;
+use panic_halt as _;
+use rp_pico::hal::prelude::*;
+use rp_pico::hal::pac;
+use rp_pico::hal;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        rp_pico::XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let sio = hal::Sio::new(pac.SIO);
+
+    let pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led_pin = pins.gpio18.into_push_pull_output();
+    let mut led_pin2 = pins.gpio19.into_push_pull_output();
+    let mut led_pin3 = pins.gpio20.into_push_pull_output();
+
+    loop {
+        led_pin.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin2.set_high().unwrap();
+        delay.delay_ms(500);
+        led_pin3.set_high().unwrap();
+        delay.delay_ms(500);
+
+        led_pin.set_low().unwrap();
+        delay.delay_ms(500);
+        led_pin2.set_low().unwrap();
+        delay.delay_ms(500);
+        led_pin3.set_low().unwrap();
+        delay.delay_ms(500);
+    }
+}
